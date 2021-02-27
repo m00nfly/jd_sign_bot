6fc1049130ff5c9829ec0d609b9c6e58ba385d53
m00nfly <cyc2008sky@gmail.com>
2021-02-28 03:25:49 +0800

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const rp = require('request-promise');
 const download = require('download');
 require('./JD_DailyBonus')
 // 公共变量
-const serverJ = '';
+//const serverJ = '';
 
 
 async function changeFile () {
